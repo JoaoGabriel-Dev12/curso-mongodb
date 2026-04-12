620a227559f39bf4047502f0a5925f8912ded377
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-04-11 21:22:15 -0300

    feat: Config class

---
+++ src/main/java/com/joaogabriel/mongoproject/config/Config.java
@@ -0,0 +1,33 @@
+package com.joaogabriel.mongoproject.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.joaogabriel.mongoproject.domain.User;
+import com.joaogabriel.mongoproject.repository.UserRepository;
+
+@Configuration
+public class Config implements CommandLineRunner{
+
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}

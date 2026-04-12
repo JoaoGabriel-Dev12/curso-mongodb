5d018d3969206853135f3b518fdfa98d323163e8
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-04-12 16:55:27 -0300

    feat: return post of a user

--- src/main/java/com/joaogabriel/mongoproject/resources/UserResource.java
+++ src/main/java/com/joaogabriel/mongoproject/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaogabriel.mongoproject.domain.Post;
 import com.joaogabriel.mongoproject.domain.User;
 import com.joaogabriel.mongoproject.dto.UserDTO;
 import com.joaogabriel.mongoproject.service.UserService;
@@ -67,5 +68,11 @@ public class UserResource {
 		
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }

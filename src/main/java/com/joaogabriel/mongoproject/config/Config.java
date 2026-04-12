89b4a1fec114fb8d1513456acbec7e90600aa02e
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-04-12 15:06:44 -0300

    feat: Post class

--- src/main/java/com/joaogabriel/mongoproject/config/Config.java
+++ src/main/java/com/joaogabriel/mongoproject/config/Config.java
@@ -1,12 +1,16 @@
 package com.joaogabriel.mongoproject.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.joaogabriel.mongoproject.domain.Post;
 import com.joaogabriel.mongoproject.domain.User;
+import com.joaogabriel.mongoproject.repository.PostRepository;
 import com.joaogabriel.mongoproject.repository.UserRepository;
 
 @Configuration
@@ -14,19 +18,30 @@ public class Config implements CommandLineRunner{
 
 	
 	@Autowired
-	private UserRepository repository;
+	private UserRepository uRepository;
+	
+	@Autowired
+	private PostRepository pRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		repository.deleteAll();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		uRepository.deleteAll();
+		pRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		repository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("12/04/2026"), "Partiu viagem!", "Vou para Goiânia!", maria);
+		Post post2 = new Post(null, sdf.parse("12/04/2026"), "No BJJ", "No pain no gain!!", maria);
+		
+		uRepository.saveAll(Arrays.asList(maria, alex, bob));
+		pRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 

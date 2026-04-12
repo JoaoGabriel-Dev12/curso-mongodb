a0f500a10cc219f37b10b13adf7940e12af4ec29
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-04-12 07:32:38 -0300

    feat: findById and ExceptionHandler

--- src/main/java/com/joaogabriel/mongoproject/resources/UserResource.java
+++ src/main/java/com/joaogabriel/mongoproject/resources/UserResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.joaogabriel.mongoproject.domain.User;
 import com.joaogabriel.mongoproject.dto.UserDTO;
 import com.joaogabriel.mongoproject.service.UserService;
 
@@ -25,5 +27,12 @@ public class UserResource {
 				.stream().map(u -> new UserDTO(u)).toList();
 		
 		return ResponseEntity.ok().body(listaDto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/joaogabriel/mongoproject/service/UserService.java
+++ src/main/java/com/joaogabriel/mongoproject/service/UserService.java
@@ -1,12 +1,14 @@
 package com.joaogabriel.mongoproject.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaogabriel.mongoproject.domain.User;
 import com.joaogabriel.mongoproject.repository.UserRepository;
+import com.joaogabriel.mongoproject.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 }

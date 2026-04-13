0e74d0d8029607d9a95744df4a240530b2edccff
Joao Gabriel <joaogabriel.dev12@gmail.com>
2026-04-12 23:07:42 -0300

    consultas com querys do mongodb

--- src/main/java/com/joaogabriel/mongoproject/resources/PostResource.java
+++ src/main/java/com/joaogabriel/mongoproject/resources/PostResource.java
@@ -1,13 +1,18 @@
 package com.joaogabriel.mongoproject.resources;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaogabriel.mongoproject.domain.Post;
+import com.joaogabriel.mongoproject.resources.util.URL;
 import com.joaogabriel.mongoproject.service.PostService;
 
 @RestController
@@ -24,5 +29,28 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		
+		return ResponseEntity.ok().body(list);
+	}
 	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate){
+		
+		text = URL.decodeParam(text);
+		
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.fullSearch(text, min, max);
+		
+		return ResponseEntity.ok().body(list);
+	}
 }
